ae851b1f4854528ad74af6df559de880276a45bc
github-action-benchmark <github@users.noreply.github.com>
2020-11-23 13:56:02 +0000

    add rt_multi_threaded (cargo) benchmark result for f927f01a34d7cedf0cdc820f729a7a6cd56e83dd

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1606139755853,
+  "lastUpdate": 1606139762454,
   "repoUrl": "https://github.com/HK416-is-all-you-need/tokio",
   "entries": {
     "sync_semaphore": [
@@ -53,6 +53,56 @@ window.BENCHMARK_DATA = {
             "name": "uncontended_concurrent_single",
             "value": 1061,
             "range": "± 34",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ],
+    "rt_multi_threaded": [
+      {
+        "commit": {
+          "author": {
+            "email": "eliza@buoyant.io",
+            "name": "Eliza Weisman",
+            "username": "hawkw"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f927f01a34d7cedf0cdc820f729a7a6cd56e83dd",
+          "message": "macros: fix rustfmt on 1.48.0 (#3160)\n\n## Motivation\r\n\r\nLooks like the Rust 1.48.0 version of `rustfmt` changed some formatting\r\nrules (fixed some bugs?), and some of the code in `tokio-macros` is no\r\nlonger correctly formatted. This is breaking CI.\r\n\r\n## Solution\r\n\r\nThis commit runs rustfmt on Rust 1.48.0. This fixes CI.\r\n\r\nCloses #3158",
+          "timestamp": "2020-11-20T10:19:26-08:00",
+          "tree_id": "bd0243a653ee49cfc50bf61b00a36cc0fce6a414",
+          "url": "https://github.com/HK416-is-all-you-need/tokio/commit/f927f01a34d7cedf0cdc820f729a7a6cd56e83dd"
+        },
+        "date": 1606139757918,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "chained_spawn",
+            "value": 190984,
+            "range": "± 14854",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ping_pong",
+            "value": 733044,
+            "range": "± 37508",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "spawn_many",
+            "value": 5367234,
+            "range": "± 626057",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "yield_many",
+            "value": 21133254,
+            "range": "± 2297565",
             "unit": "ns/iter"
           }
         ]
